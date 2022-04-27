278c1add52e261532a7097eb0464d6a670f0c84a
ElmerVargas1120 <103598358+ElmerVargas1120@users.noreply.github.com>
2022-04-27 11:30:16 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(83, 19, 148)
+
+    if(keyIsDown(DOWN_ARROW) == true){
+  background(11, 189, 245)
+}
+noStroke()
+  if(keyIsDown(DOWN_ARROW) == true){fill(223, 242, 146)
+  circle(350, 50, 100) } 
+  else{ fill(214, 253, 58)
+  circle(50, 50, 100) } 
+
+   if(keyIsDown(DOWN_ARROW) == true){ fill(5, 53 ,14)
+  square(0, 300, 500) } 
+  else{  fill(41, 201 ,36)
+  square(0, 300, 500) } 
+  
+  
+ 
+}
